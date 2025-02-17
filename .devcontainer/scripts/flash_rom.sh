42f7113db9360be2c01cfa0a6d38913ab80574ef
maxgallup <maxgallup@pm.me>
2025-02-17 16:35:12 +0100

    chore: trying to flash the damn chip

--- .devcontainer/scripts/flash_rom.sh
+++ .devcontainer/scripts/flash_rom.sh
@@ -2,7 +2,5 @@
 
 echo "todo, consolidate all into one repo"
 
-flashrom -p serprog:dev=/dev/ttyACM0:115200,spispeed=512 -r test_flash_readout.rom -V --progress
-
-
+flashrom -p serprog:dev=/dev/ttyACM0:115200,spispeed=12M -c W25Q128.W -r test_flash_readout.rom -V --progress
 
