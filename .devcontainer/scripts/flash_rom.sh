77c21db0d822333cf271628f99586b3dddc3573f
maxgallup <maxgallup@pm.me>
2025-02-18 08:27:35 +0100

    feat: reading flash contents works, cleaned contacts with isopropyl alc

--- .devcontainer/scripts/flash_rom.sh
+++ .devcontainer/scripts/flash_rom.sh
@@ -2,5 +2,24 @@
 
 echo "todo, consolidate all into one repo"
 
-flashrom -p serprog:dev=/dev/ttyACM0:115200,spispeed=12M -c W25Q128.W -r test_flash_readout.rom -V --progress
+# This script is used to write the custom coreboot image to the the connected 
+# eeprom chip.
+
+# Check if an argument was provided
+if [ $# -eq 0 ]; then
+    echo "Error: No argument provided"
+    echo "Usage: $0 [coreboot.rom]"
+    exit 1
+fi
+
+
+# Write command
+flashrom -p serprog:dev=/dev/ttyACM0:115200,spispeed=12M -c W25Q128.W -w $1 -V --progress
+
+# Read command
+# flashrom -p serprog:dev=/dev/ttyACM0:115200,spispeed=12M -c W25Q128.W -r test_flash_readout.rom -V --progress
+
+
+
+
 
