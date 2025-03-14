3e482dd3468d25ac403b7231aec79b059e3fea09
maxgallup <maxgallup@pm.me>
2025-03-14 09:56:43 +0100

    chore: build script edit

--- .devcontainer/scripts/configure-and-build.sh
+++ .devcontainer/scripts/configure-and-build.sh
@@ -95,53 +95,21 @@ function configure_supermicro() {
     ./util/scripts/config --enable CONFIG_VENDOR_SUPERMICRO
     ./util/scripts/config --enable CONFIG_USE_LEGACY_8254_TIMER
     ./util/scripts/config --enable CONFIG_HAVE_IFD_BIN
-    ./util/scripts/config --set-str CONFIG_IFD_BIN_PATH "extracted/supermicro/flashregion_0_flashdescriptor.bin"
     ./util/scripts/config --enable CONFIG_BOARD_SUPERMICRO_X11SSH_F
     ./util/scripts/config --enable CONFIG_HAVE_ME_BIN
+    ./util/scripts/config --set-str CONFIG_IFD_BIN_PATH "extracted/supermicro/flashregion_0_flashdescriptor.bin"
     ./util/scripts/config --set-str CONFIG_ME_BIN_PATH "extracted/supermicro/flashregion_2_intel_me.bin"
 
-    # Console related tests
-    ./util/scripts/config --enable CONFIG_DEFAULT_CONSOLE_LOGLEVEL_7
-    
-    # CONFIG_UART_FOR_CONSOLE 0 is for COM1, COM2 doesn't work :(
-    ./util/scripts/config --set-val CONFIG_UART_FOR_CONSOLE 0
+    ./util/scripts/config --enable  CONFIG_ONBOARD_VGA_IS_PRIMARY
+    ./util/scripts/config --enable  CONFIG_HAVE_VGA_TEXT_FRAMEBUFFER
+    ./util/scripts/config --enable  CONFIG_VGA_TEXT_FRAMEBUFFER
+    ./util/scripts/config --enable  CONFIG_VGA
 
-    # We ask to load coreboot, but in bs_payload_load we take over execution
+    # Coreinfo Payload
     ./util/scripts/config --enable CONFIG_PAYLOAD_ELF
     ./util/scripts/config --set-str CONFIG_PAYLOAD_FILE "payloads/coreinfo/build/coreinfo.elf"
 
-    # SeaBios Payload - didn't work
-    # ./util/scripts/config --enable CONFIG_PAYLOAD_SEABIOS
-    # ./util/scripts/config --enable CONFIG_PAYLOAD_BUILD_SEABIOS
-    # ./util/scripts/config --enable CONFIG_COMPRESSED_PAYLOAD_LZMA
-    # ./util/scripts/config --enable CONFIG_COMPRESS_SECONDARY_PAYLOAD
-    # ./util/scripts/config --enable CONFIG_SEABIOS_THREAD_OPTIONROMS
-    # ./util/scripts/config --set-str CONFIG_PAYLOAD_FILE "payloads/external/SeaBIOS/seabios/out/bios.bin.elf"
-
-    # TianoCore EDK2 - didn't work
-    # ./util/scripts/config --enable CONFIG_WANT_LINEAR_FRAMEBUFFER
-    # ./util/scripts/config --enable CONFIG_GENERIC_LINEAR_FRAMEBUFFER
-    # ./util/scripts/config --enable CONFIG_LINEAR_FRAMEBUFFER
-    # ./util/scripts/config --enable CONFIG_PAYLOAD_EDK2
-    # ./util/scripts/config --enable CONFIG_EDK2_RELEASE
-    # ./util/scripts/config --enable CONFIG_EDK2_FULL_SCREEN_SETUP
-    # ./util/scripts/config --enable CONFIG_EDK2_HAVE_EFI_SHELL
-    # ./util/scripts/config --enable CONFIG_EDK2_PRIORITIZE_INTERNAL
-    # ./util/scripts/config --enable CONFIG_EDK2_PS2_SUPPORT
-    # ./util/scripts/config --enable CONFIG_EDK2_UEFIPAYLOAD
-    # ./util/scripts/config --enable CONFIG_EDK2_REPO_MRCHROMEBOX
-    # ./util/scripts/config --set-str CONFIG_PAYLOAD_FILE "build/UEFIPAYLOAD.fd"
-    # ./util/scripts/config --set-str CONFIG_EDK2_REPOSITORY "https://github.com/mrchromebox/edk2"
-    # ./util/scripts/config --set-str CONFIG_EDK2_TAG_OR_REV "origin/uefipayload_202309"
-    # ./util/scripts/config --set-str CONFIG_EDK2_BOOTSPLASH_FILE "Documentation/coreboot_logo.bmp"
-    # ./util/scripts/config --set-str CONFIG_EDK2_CUSTOM_BUILD_PARAMS ""
-    # ./util/scripts/config --set-val CONFIG_EDK2_BOOT_TIMEOUT 2
-    # ./util/scripts/config --set-val CONFIG_EDK2_SD_MMC_TIMEOUT 10
-
     make olddefconfig
-
-    # Run test
-    # qemu-system-x86_64 -bios build/coreboot.rom -serial stdio
 }
 
 
@@ -150,24 +118,25 @@ function build() {
     make
 }
 
+if [[ "$2" == "--init" ]]; then
+    first_time_init
+fi
 
 if [[ "$1" == "qemu" ]]; then
-    first_time_init
     configure_qemu
-    build
 elif [[ "$1" == "upsquared" ]]; then
-    first_time_init
     configure_upsquared
-    build
 elif [[ "$1" == "supermicro" ]]; then
-    first_time_init
     configure_supermicro
-    build
 else
     # Help: Any other argument passed
-    echo "Usage: $(basename $0) [--init|upsquared|supermicro]"
+    echo "Usage: $(basename $0) [qemu|upsquared|supermicro] [--init]"
     echo "  upsquared:    config for UP Squared hardware"
     echo "  supermicro:   config for Supermicro hardware"
     echo "  qemu:         config for QEMU example"
     exit 1
 fi
+
+build
+
+echo "Done building: coreboot/build/coreboot.rom"
